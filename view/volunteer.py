592e0c575935131e300ad7f057977847f2ed79d3
orertrr <orertrr6207@gmail.com>
2024-07-14 23:34:18 +0800

    feat: add api for registering COSCUP-Volunteer user as subscriber
    Signed-off-by: orertrr <orertrr6207@gmail.com>

--- view/volunteer.py
+++ view/volunteer.py
@@ -1,6 +1,46 @@
 '''
 API for COSCUP-Volunteer
 '''
-from flask import Blueprint
+import re
 
-VIEW_VOLUNTEER = Blueprint('volunteer', __name__, url_prefix='/coscup')
+from flask import Blueprint, make_response, Response, request
+from werkzeug.exceptions import BadRequest
+
+from module.subscriber import Subscriber
+
+VIEW_VOLUNTEER = Blueprint('volunteer', __name__, url_prefix='/volunteer')
+
+EMAIL_REGEX = re.compile(r'[^@]+@[^@]+\.[^@]+')
+
+@VIEW_VOLUNTEER.errorhandler(BadRequest)
+def handle_bad_request(e: BadRequest) -> Response:
+    return make_response({ 'message': e.description }, e.code)
+
+@VIEW_VOLUNTEER.route('/subscriber', methods=('POST',))
+def add_subscriber() -> Response:
+    '''API for add subscriber from COSCUP-Volunteer
+
+    Request Body: 
+    {
+        "name": string,
+        "email": string
+    }
+    '''
+    add_subscriber_dto = request.get_json()
+    if 'name' not in add_subscriber_dto or \
+        type(add_subscriber_dto['name']) is not str:
+        raise BadRequest('name should be in the request body and string')
+    
+    if 'email' not in add_subscriber_dto or \
+        type(add_subscriber_dto['email']) is not str:
+        raise BadRequest('email should be in the request body and string')
+    
+    if not EMAIL_REGEX.match(add_subscriber_dto['email']):
+        raise BadRequest('The email is not valid')
+    
+    Subscriber.process_upload(
+        name=add_subscriber_dto['name'],
+        mail=add_subscriber_dto['email']
+    )
+
+    return make_response({ 'success': True })
